31ce32070f7df34ff5f1c959d7eb1988375f4234
Kunvar13 <kunvar13@gmail.com>
2022-02-28 22:51:12 -0600

    Adding test case

---
+++ FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddUnitTesting
+{
+    public class FizzBuzz
+    {
+        public static string FizzBuzzMethod(int x)
+        {
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                return "FizzBuzz";
+            } 
+            else if (x % 3 == 0)
+            {
+                return "Fizz";
+            } 
+            else if (x % 5 == 0 )
+            {
+                return "Buzz";
+            } else
+            {
+                return "Not a FizzBuzz Number";
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//using FizzBuzz;
+
+namespace TddUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number");
+            string y  = Console.ReadLine();
+            Console.WriteLine(FizzBuzz.FizzBuzzMethod(Convert.ToInt32(y)));
+        }
+    }
+}
